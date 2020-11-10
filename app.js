5de598423db2a510f9a9d04271e50e745cb75224
Micah Lund <micah.r.lund@gmail.com>
2020-11-10 12:59:09 -0700

    Made a call to the api using fetch and then, access was blocked due to using localhost so we need a workaround.

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ window.addEventListener('load', () => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/37.8267,-122.4233`;
+            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`;
+
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
         })
+
     } else {
         h1.textContent = "Please enable us to know your location."
     }
