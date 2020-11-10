99e782a256f682ff4d7426d3dd53bdbdae450938
Micah Lund <micah.r.lund@gmail.com>
2020-11-10 09:54:04 -0700

    Began writing the JS logic for the project.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//JS file for project
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+        })
+    } else {
+        h1.textContent = "Please enable your location"
+    }
+})
