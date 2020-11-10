3aba5a1f6a69b8833c47ffc8426a4586b0820542
Micah Lund <micah.r.lund@gmail.com>
2020-11-10 13:54:55 -0700

    Set up query selectors to display elements to the DOM.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescr = document.querySelector('.temperaure-description');
+    let temperatureDegree = document.querySelector('.temperaure-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            console.log(position);
 
             const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1a302185f4bd5af754e2e38318c2233d`
 
@@ -15,6 +20,8 @@ window.addEventListener('load', () => {
             })
             .then(data => {
                 console.log(data);
+                const {temp, name} = data.main;
+                const {main} = data.weather[0];
             })
         })
 
