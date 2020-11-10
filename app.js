c0080d458e7c1d56d7b6d06862830001389dcb80
Micah Lund <micah.r.lund@gmail.com>
2020-11-10 10:44:31 -0700

    Added API call as per what is being used in the tutorial. Dark Sky is no longer accepting new accounts so in order to utilize the geolocation functionality I am going to use the instructor's call. If there are issues I will revamp the project to a city and state method.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener('load', () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/37.8267,-122.4233`;
         })
     } else {
         h1.textContent = "Please enable us to know your location."
